6384ed14a90439981e55284a86f9a66bd399dc62
Knut Lorvik <lorviken@gmail.com>
2015-04-25 00:37:07 +0200

    Update config path

--- rabbitmq-wrapper.sh
+++ rabbitmq-wrapper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sed -i "21s|.*|    {default_user, <<\"$RABBITMQ_USERNAME\">>},|" config/rabbitmq.config
-sed -i "22s|.*|    {default_pass, <<\"$RABBITMQ_PASSWORD\">>},|" config/rabbitmq.config
+sed -i "21s|.*|    {default_user, <<\"$RABBITMQ_USERNAME\">>},|" /etc/rabbitmq/rabbitmq.config
+sed -i "22s|.*|    {default_pass, <<\"$RABBITMQ_PASSWORD\">>},|" /etc/rabbitmq/rabbitmq.config
 
 # Enable management
 rabbitmq-plugins enable rabbitmq_management --offline
