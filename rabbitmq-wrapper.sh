f89c3b0fed019d3de429d54ed6dbf7d240fa6880
Knut Lorvik <lorviken@gmail.com>
2015-04-28 11:55:18 +0200

    Move clusterification to seperate function

--- rabbitmq-wrapper.sh
+++ rabbitmq-wrapper.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+function clusterize {
+    sleep 10
+    rabbitmqctl stop_app
+    rabbitmqctl join_cluster rabbit@$CLUSTER_WITH
+    rabbitmqctl start_app
+}
+
 sed -i "21s|.*|    {default_user, <<\"$RABBITMQ_USERNAME\">>},|" /etc/rabbitmq/rabbitmq.config
 sed -i "22s|.*|    {default_pass, <<\"$RABBITMQ_PASSWORD\">>},|" /etc/rabbitmq/rabbitmq.config
 
 # Enable management
-#rabbitmq-plugins enable rabbitmq_management --offline
+rabbitmq-plugins enable rabbitmq_management --offline
 
 # Start server
 if [ -z "$CLUSTER_WITH" ] ; then
     rabbitmq-server
 else
-    rabbitmq-server &
-    sleep 10
-    rabbitmqctl stop_app
-    rabbitmqctl join_cluster rabbit@$CLUSTER_WITH
-    rabbitmqctl start_app
+    clusterize &
+    rabbitmq-server
 fi
